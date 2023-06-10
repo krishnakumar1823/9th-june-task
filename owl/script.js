b1cd0ac478b34df028cfaf4cc708d7a2ce29ec14
krishnakumar1823 <kumarkrishna180320@gmail.com>
2023-06-10 23:21:44 +0530

    add

---
+++ owl/script.js
@@ -0,0 +1,32 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3500,
+    autoplayHoverPause:true,
+	responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+//shortcut check js is linked or not
+console.log("krish")
+//
